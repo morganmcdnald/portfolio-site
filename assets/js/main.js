ad469c1e7dd59ff8de16858a1bfdfed71cb4c0cc
Morgan McDonald <morganmcdee13@outlook.com>
2021-01-12 10:51:46 +0000

    Reduce header size on scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,6 +50,7 @@ window.addEventListener('scroll', scrollActive)
 function scrollHeader() {
   const header = document.getElementById('header')
   if (this.scrollY >= 200) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+  // if (this.scrollY >= 200) header.classList.add('nav__menu-scroll'); else header.classList.remove('nav__menu-scroll')
 }
 
 window.addEventListener('scroll', scrollHeader)
